b9593275cb4f05f7d7a10ebacb26e9fef68ec1e0
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2011-11-29 04:19:19 +0000

    0.1 top3proc
    git-svn-id: https://172.29.254.10:3000/svn/projects/smarteye-plugin/trunk@49 2e5cfa46-41e0-4c30-b113-11416295e233

---
+++ top3proc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "usage: $0 [cpu|mem]"
+    exit 99
+fi
+
+case $1 in
+    cpu) col=7 ;;
+    mem) col=6 ;;
+    *)   echo "parameter error"
+         exit 99
+    ;;
+esac
+
+/bin/ps axwo 'stat uid pid ppid vsz rss pcpu comm args' >/tmp/$$
+
+cat /tmp/$$ |sort -k${col}rn |head -n 3 |awk '{printf "%s/%s\n",$3,$8}' |xargs
+
+#rm -f /tmp/$$
+
