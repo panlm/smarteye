5eca44efd06911c2bae090e114e9765b76539d05
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2011-12-23 07:28:41 +0000

    minor edit
    git-svn-id: https://172.29.254.10:3000/svn/projects/smarteye-plugin/trunk@157 2e5cfa46-41e0-4c30-b113-11416295e233

--- check_snmp_load.pl
+++ check_snmp_load.pl
@@ -1,4 +1,4 @@
-#!/usr/local/smarteye/perl/bin/perl -w 
+#!/usr/local/groundwork/perl/bin/perl -w 
 ############################## check_snmp_load #################
 # Version : 1.3.1
 # Date : 8 Sept 2006
@@ -18,7 +18,7 @@ use Getopt::Long;
 
 # Nagios specific
 
-use lib "/usr/local/smarteye/nagios/libexec";
+use lib "/usr/local/groundwork/nagios/libexec";
 use utils qw(%ERRORS $TIMEOUT);
 #my $TIMEOUT = 15;
 #my %ERRORS=('OK'=>0,'WARNING'=>1,'CRITICAL'=>2,'UNKNOWN'=>3,'DEPENDENT'=>4);
