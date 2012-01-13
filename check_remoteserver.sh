5a7fee587d454933ac31f7a90209dc5a5b27571d
tianbz <tianbz@2e5cfa46-41e0-4c30-b113-11416295e233>
2012-01-13 07:52:43 +0000

    remoteServer 心跳脚本
    git-svn-id: https://172.29.254.10:3000/svn/projects/smarteye-plugin/trunk@232 2e5cfa46-41e0-4c30-b113-11416295e233

---
+++ check_remoteserver.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#ips=(`ifconfig | grep 'inet addr' | awk -F ':' '{print $2}' | grep --color -o '\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}'`)
+
+ips=(`ifconfig -a | sed -n '/^[^ \t]/{N;s/\(^[^ ]*\).*addr:\([^ ]*\).*/\1\t\2/p}'|grep -v '127.0.0.1'`)
+
+
+message=""
+for ((i=0;i<${#ips[@]};i++));
+do
+	message="$message ${ips[i]}"
+	
+done
+
+echo "OK - RemoterServer is ok , ip :$message  | status=0;;;;"
+
+exit 0
+
+
+
