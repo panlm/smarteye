b9fa6d53dc155116909a26ee87df0aeb21d312a4
stevenpan <stevenpan@2e5cfa46-41e0-4c30-b113-11416295e233>
2012-02-20 01:37:54 +0000

    for Yun Da Project
    git-svn-id: https://172.29.254.10:3000/svn/projects/smarteye-plugin/trunk@275 2e5cfa46-41e0-4c30-b113-11416295e233

---
+++ check_uptime.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "usage: $0 hostname snmp-community-string"
+    exit 99
+fi
+
+host=$1
+comm=$2
+out=/var/tmp/check_uptime.$host.last
+
+if [ ! -f $out ]; then
+    echo 0 > $out
+fi
+
+a=$(snmpwalk -v2c -c$comm $host .1.3.6.1.2.1.1.3.0 2>&1)
+if [ $? -ne 0 ]; then
+    echo "snmp get error"
+    echo $a
+fi
+
+oldtime=$(cat $out)
+time=$(echo $a |awk -F'[()]' '{print $2}')
+
+echo $time >$out
+echo $a |awk -F'[()]' '{print "uptime:",$3}'
+
+if [ $time -gt $oldtime ]; then
+    exit 0
+else
+    exit 2
+fi
